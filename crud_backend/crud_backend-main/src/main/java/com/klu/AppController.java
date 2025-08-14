41c5811cd1f96faa6f710feee818dd4aae52d0e2
Dunaka Chetan <chetan521230@gmail.com>
2025-08-14 12:26:04 +0530

    Update AppController.java

--- crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
+++ crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
@@ -33,11 +33,9 @@ public class AppController {
 		return s.updateData(p);
 	}
 
-	/* 
 	@DeleteMapping("/delete/{id}")
 	public String delete(@PathVariable int id) {
 		return s.deleteData(id);
 	}
-	*/
 	
 }
